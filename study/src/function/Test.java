fd1919359ef7e8b92a1e404a5f256c487a7ebb11
guli52 <mil960802@naver.com>
2021-03-31 19:50:41 +0900

    백준15596-3/31

---
+++ study/src/function/Test.java
@@ -0,0 +1,17 @@
+package function;
+
+public class Test {
+	/*
+	long sum(int[] a); (클래스 이름: Test)
+	a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+	리턴값: a에 포함되어 있는 정수 n개의 합
+	*/
+	
+	long sum(int[] a) {
+		long b = 0;
+		for(int i=0; i<a.length; i++) {
+			b += a[i];
+		}
+		return b;
+	}
+}
